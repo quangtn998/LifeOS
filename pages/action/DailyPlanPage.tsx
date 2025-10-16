cd4f436bce7cfa094c836aff64c96ff90479bca7
quangtrungnguyen1122 <ngocquangtrungnguyen@gmail.com>
2025-10-16 17:09:30 +0700

    Updated DailyPlanPage.tsx

--- pages/action/DailyPlanPage.tsx
+++ pages/action/DailyPlanPage.tsx
@@ -271,6 +271,7 @@ const DailyPlanPage: React.FC = () => {
 
 const getStartOfWeek = (date: Date) => {
     const d = new Date(date);
+    d.setHours(0, 0, 0, 0);
     const day = d.getDay();
     const diff = d.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
     return new Date(d.setDate(diff));
